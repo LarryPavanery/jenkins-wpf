0e909f5078edb6272388fcfc2c406a1ec8166835
Larry Pavanery <larry_expert@hotmail.com>
2015-10-30 15:12:42 -0300

    Create tests NUnit

---
+++ IntroTests/FirstTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IntroTest
+{
+    [TestFixture]
+    class FirstTests
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            int x = 2;
+            int y = 2;
+
+            Assert.AreEqual(x, y);
+        }
+
+        [Test]
+        public void NegativeTest()
+        {
+            if (true)
+            {
+                Assert.Fail("!!!This is a failure, oh no!!! =D");
+            }
+        }
+
+        [Test, ExpectedException(typeof(NotSupportedException))]
+        public void ExceptedExceptionTest()
+        {
+            throw new NotSupportedException();
+        }
+
+        [Test, Ignore]
+        public void NotImplementedException()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IntroTests/SecondTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IntroTest
+{
+    [TestFixture]
+    class SecondTests
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [Test]
+        public void NegativeTest()
+        {
+            Assert.Fail("This is a failure in SecondTests");
+        }
+    }
+}

a6568bf179937b5393226d706e2c919006a98823
Larry Pavanery <larry_expert@hotmail.com>
2015-10-30 13:44:53 -0300

    create class calc and add test to method sum

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calc
+    {
+        public int MyPropertyInt { get; set; }
+        public string MyPropertyString { get; set; }
+
+        public int soma(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ClassLibrary1Tests/Class1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void somaTest()
+        {
+            Calc cl = new Calc();
+            Assert.AreEqual(4, cl.soma(2, 2), "x:<{0}> * y:<{1}> = 4", new object[] { 2, 2 });
+        }
+    }
+}
